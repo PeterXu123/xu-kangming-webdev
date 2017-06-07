4fd5f3af632bc58019ae3d49dd88167ea18fbef7
PeterXu123 <xukangming111@gmail.com>
2017-06-06 21:49:23 -0400

    htrh
    htrhtr

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var app = express();
+var app = require('./express');
 
+require('./utilities/filelist');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/public'));
+app.use(app.express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
+require('./assignment/app')(app);
+ // require('public/assignment/directives/app.js')
+
+
 
 var port = process.env.PORT || 3000;
 
