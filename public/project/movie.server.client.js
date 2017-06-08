26227c2f86eeb848fe3c2f86c14cbfd2760731ce
PeterXu123 <xukangming111@gmail.com>
2017-06-08 12:45:27 -0400

    yuh

--- public/project/movie.server.client.js
+++ public/project/movie.server.client.js
@@ -39,8 +39,8 @@
         //
         function findMovieById(movieId) {
            //  var url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=b98a14ef11eb47f3e7515a284f1a6371&language=en-US;"
-           var url = "http://www.omdbapi.com/?apikey=8e5e4416&i=" + movieId;
-            return $http.get(url)
+           var url = "https://www.omdbapi.com/?apikey=8e5e4416&i=" + movieId;
+            return $https.jsonp($sce.trustAsResourceUrl(url))
                 .then(function (response) {
                     return response.data;
                 })
